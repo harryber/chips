5b3e804bfad794275ddfa16e7fc12f030e7f4065
Harry <44312007+harryber@users.noreply.github.com>
2024-12-03 20:02:33 -0800

    some changes

--- script.js
+++ script.js
@@ -1,65 +1,45 @@
 // Initialize pot and player stacks
 
-/*
-todo list:
-
-[x] fix call for blinds
-[x] fix call for when you have already put money (keep track of how much each player has already put in)
-[x] only display buttons for current player
-[x] add fold button
-[x] add check button to the call button
-[x] create add player button
-[x] create remove player button
-[ ] support removing specific player
-[ ] change player names
-[ ] add rounds (preflop, flop, turn, river)
-[ ] resolve side pots
-*/
-
-
 const SMALL_BLIND = 5;
 const BIG_BLIND = 10;
 const STARTING_STACK = 1000;
 
+const FULL_RESET = 1;
+const NEXT_CARD_RESET = 0;
+
 let pot = 0;
 let prevBet = 0;
+let raiser = -1;
 let dealer = 0;
 let turn = 1;
-let numPlayers = 4;
+let numPlayers = 1;
 
 let playerStacks = {
-    player1: STARTING_STACK,
-    player2: STARTING_STACK,
-    player3: STARTING_STACK,
-    player4: STARTING_STACK
+    player1: STARTING_STACK
 };
 
 let alreadyAdded = {
-    player1: 0,
-    player2: 0,
-    player3: 0,
-    player4: 0
+    player1: 0
 }
 
 let folded = {
-    player1: false,
-    player2: false,
-    player3: false,
-    player4: false
+    player1: false
 }
 
 // Function to add to the pot (deduct from the player's stack)
 function raise(playerNum) {
-    const player = "player" + playerNum;
+    const playerid = "player" + playerNum;
 
     const amount = parseInt(prompt("Raise amount:"));
 
     // support the cancel button, do not allow bad input
     if (amount == null || amount < 0) return;
 
-    if (playerStacks[player] >= amount) {
+    if (playerStacks[playerid] >= amount) {
         if (amount >= prevBet * 2) {
-            prevBet = amount + alreadyAdded[player];
+            // raise goes through
+            raiser = playerNum;
+            prevBet = amount + alreadyAdded[playerid];
             put(playerNum, amount);
             nextTurn();
            
@@ -83,42 +63,81 @@ function put(playerNum, amount) {
 }
 
 function callBet(playerNum) {
-    const player = "player" + playerNum;
+    const playerid = getPlayerID(playerNum);
 
 
-    let amount = prevBet - alreadyAdded[player];
+    let amount = prevBet - alreadyAdded[playerid];
 
     put(playerNum, amount);
-    nextTurn();
+
+    checkForEndOfTurn();
 }
 
 function fold(playerNum) {
-    folded["player" + playerNum] = true;
-    nextTurn();
+    folded[getPlayerID(playerNum)] = true;
+    
+    checkForEndOfTurn();
+}
+
+// NEED TO ALLOW FOR THE LAST PERSON TO RAISE
+function checkForEndOfTurn() {
+    let nextTurnNum = turn + 1 > numPlayers ? 1 : turn + 1;
+    console.log(`Turn: ${turn}, Raiser: ${raiser}`);
+
+    if (nextTurnNum == raiser) {
+        window.alert("Deal next card(s)!");
+        resetRound(NEXT_CARD_RESET);
+    } else {
+        nextTurn();
+    }
+    
 }
 
 function pay(playerNum) {
-    const player = "player" + playerNum;
+    const playerid = getPlayerID(playerNum);
 
-    playerStacks[player] += pot;
+    playerStacks[playerid] += pot;
     pot = 0;
 
     renderStackandPot(playerNum);
 
     // disable buttons
     removeButtons(turn);
+
+    // enable add/remove players
+    enableAddRemovePlayers();
+
+    resetRound(FULL_RESET);
 }
 
-function resetRound() {
-    for (let i = 1; i <= numPlayers; i++) {
-        amountToAdd["player" + i] = 0;
-        folded["player" + i] = false;
+function resetRound(full) {
+    prevBet = 0;
+    if (full) {
+        for (let i = 1; i <= numPlayers; i++) {
+            alreadyAdded[getPlayerID(i)] = 0;
+            folded[getPlayerID(i)] = false;
+        }
+    } else {
+        // set current turn to dealer + 1
+        removeButtons(turn);
+        turn = dealer + 1 > numPlayers ? 1 : dealer + 1;
+        raiser = dealer;
+        addButtons(turn);
+
+        // players have added 0 to the pot so far
+        for (let i = 1; i <= numPlayers; i++) {
+            alreadyAdded[getPlayerID(i)] = 0;
+        }
     }
+    
 }
 
 function blinds() {
     // don't start a pot while one's already going
     if (pot != 0) return;
+
+    // disable add/remove players
+    disableAddRemovePlayers();
 
     nextDealer();
     turn = dealer;
@@ -136,8 +155,10 @@ function blinds() {
 
     nextTurn();
    
-    small = "player" + playerNumSmall;
-    big = "player" + playerNumBig;
+    small = getPlayerID(playerNumSmall);
+    big = getPlayerID(playerNumBig);
+
+    raiser = playerNumBig;
 
     put(playerNumSmall, SMALL_BLIND);
     put(playerNumBig, BIG_BLIND);
@@ -145,7 +166,7 @@ function blinds() {
 
 
 function renderStackandPot(playerNum) {
-    document.getElementById("stack" + playerNum).value = playerStacks["player" + playerNum];
+    document.getElementById("stack" + playerNum).value = playerStacks[getPlayerID(playerNum)];
     document.getElementById("pot").textContent = pot;
 }
 
@@ -177,8 +198,8 @@ function nextDealer() {
     dealerButton.id = "dealer";
 
     // Find the player and append the dealer button
-    let player = document.getElementById("player" + dealer);
-    player.appendChild(dealerButton);
+    let dealerContainer = document.getElementById("dealer-container" + dealer);
+    dealerContainer.appendChild(dealerButton);
 
 }
 
@@ -193,6 +214,7 @@ function addButtons(playerNum) {
     // Create the raise button
     let raiseButton = document.createElement("button");
     raiseButton.textContent = "+";
+    raiseButton.classList.add("optionButton");
     raiseButton.onclick = function() {
         raise(playerNum);
     };
@@ -201,6 +223,7 @@ function addButtons(playerNum) {
     let callButton = document.createElement("button");
     callButton.textContent = "-";
     callButton.classList.add("callButton");
+    callButton.classList.add("optionButton");
     callButton.onclick = function() {
         callBet(playerNum);
     };
@@ -209,6 +232,7 @@ function addButtons(playerNum) {
     let foldButton = document.createElement("button");
     foldButton.textContent = "x";
     foldButton.classList.add("foldButton");
+    foldButton.classList.add("optionButton");
     foldButton.onclick = function() {
         fold(playerNum);
     };
@@ -221,50 +245,65 @@ function addButtons(playerNum) {
   }
 
   function addPlayer() {
+    if (pot != 0) return;
+
     console.log("Player being added");
     numPlayers++;
-    let playerNumber = numPlayers;
-    let playerid = `player${playerNumber}`;
+    let playerNum = numPlayers;
+    let playerid = getPlayerID(playerNum);
     let containerId = "player-container";
+    let payContainerId = "pay-container";
     // The dynamic HTML string to be added
     const playerHTML = `
       <div class="player" id="${playerid}">
-        <label class="" id="l${playerNumber}">Player ${playerNumber} Stack: $</label>
-        <input type="number" id="stack${playerNumber}" value="1000">
-        <div class="buttcont" id="button-container${playerNumber}"></div>
+        <div class="buttcont" id="dealer-container${playerNum}"></div>
+        <label id="l${playerNum}">Player ${playerNum} Stack: $</label>
+        <input type="number" id="stack${playerNum}" value="${STARTING_STACK}">
+        <div class="buttcont" id="button-container${playerNum}"></div>
       </div>
     `;
+
+    const payButtonHTML = `
+        <button id="pay${playerNum}" class="payButton" onclick="pay(${playerNum})">${playerNum}</button>
+    `
   
     // Find the container element by ID
-    const container = document.getElementById(containerId);
-    if (container) {
+    const playerContainer = document.getElementById(containerId);
+    const payContainer = document.getElementById(payContainerId);
+
+    if (playerContainer && payContainer) {
         // Append the HTML to the container
-        container.insertAdjacentHTML('beforeend', playerHTML);
-        console.log(`Player ${playerNumber} HTML added to container with ID: ${containerId}`);
+        playerContainer.insertAdjacentHTML('beforeend', playerHTML);
+        console.log(`Player ${playerNum} HTML added to container with ID: ${containerId}`);
         
         playerStacks[playerid] = STARTING_STACK;
         alreadyAdded[playerid] = 0;
         folded[playerid] = false;
 
+        payContainer.insertAdjacentHTML('beforeend', payButtonHTML);
 
     } else {
-        console.error(`Container with ID "${containerId}" not found.`);
+        console.error(`Container with ID "${containerId}" or "${payContainerId}" not found.`);
     }
   }
 
 function removePlayer() {
+    if (pot != 0) return;
+
     console.log("Player being added");
     
-    let playerNumber = numPlayers;
-    let playerid = `player${playerNumber}`;
-    let containerId = "player-container";
+    let playerNum = numPlayers;
+    let playerid = getPlayerID(playerNum);
+    let payid = `pay${playerNum}`;
   
     // Find the player element by ID
     const playerHTML = document.getElementById(playerid);
-    if (playerHTML) {
+    const payButtonHTML = document.getElementById(payid);
+    if (playerHTML && payButtonHTML) {
         // Append the HTML to the container
         playerHTML.remove();
-        console.log(`Player ${playerNumber} HTML removed`);
+        payButtonHTML.remove();
+        console.log(`Player ${playerNum} HTML removed`);
         
         delete playerStacks[playerid];
         delete alreadyAdded[playerid];
@@ -272,6 +311,35 @@ function removePlayer() {
         numPlayers--;
 
     } else {
-        console.error(`Player with ID "${playerid}" not found.`);
+        console.error(`Player with ID "${playerid}" or "${payid}" not found.`);
     }
+}
+
+function enableAddRemovePlayers() {
+    const containerid = "add-remove-player-container";
+    const container = document.getElementById(containerid);
+
+    if (container) {
+        container.innerHTML = `
+            <button onclick="addPlayer()">Add Player</button>
+            <button class="foldButton" onclick="removePlayer()">Remove Player</button>
+        `;
+    } else {
+        console.error(`Add and Remove container not found`);
+    }
+}
+
+function disableAddRemovePlayers() {
+    const containerid = "add-remove-player-container";
+    const container = document.getElementById(containerid);
+
+    if (container) {
+        container.innerHTML = "";
+    } else {
+        console.error(`Add and Remove container not found`);
+    }
+}
+
+function getPlayerID(playerNum) {
+    return `player${playerNum}`;
 }
